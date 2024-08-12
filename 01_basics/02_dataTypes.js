1d00e413b73dbb13234bc33eb1a4fb791ecd07ac
JyotiPrajapati37 <86409766+JyotiPrajapati37@users.noreply.github.com>
2024-08-12 08:18:03 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are using nodejs, not brower
+
+console.log(3
+    +
+    3)//code readbility sholudbe high
+
+   console.log("Hitesh") 
+
+   let  name = "Hitesh"
+   let age = 18
+   let isLoggedIn = false
+   let state;
+   //number => to power 53
+   //bigint
+   //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined=>
+    // symbol => unique
+    
+    //onject
+    console.log(typeof undefined); //undefined
+    console.log(typeof null);//object
